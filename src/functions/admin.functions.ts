a6c5e056abaf3f97ed1ec90cb78dcd308b205394
Dillon Powers <dillon.powers@overweightfinancials.com>
2026-06-15 19:55:10 -0400

    fix: remove unused server import from admin functions causing build crash

--- src/functions/admin.functions.ts
+++ src/functions/admin.functions.ts
@@ -1,5 +1,4 @@
 import { createServerFn } from "@tanstack/react-start";
-import { getRequestHeader } from "@tanstack/react-start/server";
 import { getAdminStats } from "../services/admin.service";
 
 export const getAdminData = createServerFn({ method: "GET" }).handler(
