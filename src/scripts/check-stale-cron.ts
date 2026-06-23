08c4142442ee9183db396cc93b390e289377be95
Dillon Powers <dillon.powers@overweightfinancials.com>
2026-06-23 08:40:54 -0400

    chore: remove debug scripts

--- src/scripts/check-stale-cron.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../db/index.server";
-import { cronLogs } from "../db/schema";
-import { desc } from "drizzle-orm";
-
-async function run() {
-  const latest = await db.select().from(cronLogs).orderBy(desc(cronLogs.ranAt)).limit(5);
-  console.log(latest);
-}
-run().catch(console.error).finally(() => process.exit(0));
